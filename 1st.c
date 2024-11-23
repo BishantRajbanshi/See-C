55270cfa9615e68bac8d472d315e8563b4f5d577
BishantRajbanshi <rajbancbishant@gmail.com>
2024-11-23 15:42:36 +0545

    prac

---
+++ 1st.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+
+// int main(){
+//   int a,b,sum;
+
+//   printf("Enter the value of a and b : ");
+//   scanf("%d%d",&a,&b);
+
+//   printf("Hello world");
+//   sum = a+b;
+
+//   printf("The sum of %d and %d is : %d",a,b,sum);
+//   return 0;
+// }
+
+
+// int main(){
+//   int p,t;
+//   float r,i;
+
+//   printf("Enter the value of p, t and r : ");
+//   scanf("%d%d%f",&p,&t,&r);
+
+//   i = (p*t*r)/100;
+//   printf("The simple interest is : %f",i);
+// }
+
+
+//   int main() {
+//     float c, f;
+//     int ch;
+
+//     printf("Enter 1 for Celsius to Fahrenheit or 2 for Fahrenheit to Celsius: ");
+//     scanf("%d", &ch);
+
+//     if (ch == 1) {
+//         printf("Enter the temperature in Celsius: ");
+//         scanf("%f", &c);
+//         f = c * 9 / 5 + 32;
+//         printf("%.2f Celsius is %.2f Fahrenheit.\n", c, f);
+//     } else if (ch == 2) {
+//         printf("Enter the temperature in Fahrenheit: ");
+//         scanf("%f", &f);
+//         c = (f - 32) * 5 / 9;
+//         printf("%.2f Fahrenheit is %.2f Celsius.\n", f, c);
+//     } else {
+//         printf("Invalid choice. Please enter 1 or 2.\n");
+//     }
+
+//     return 0;
+// }
+
+
+// int main(){
+//   int a;
+
+//   printf("Enter the number: ");
+//   scanf("%d",&a);
+
+//   if (a % 2 == 0)
+//   {
+//     printf("Its even number");
+//   }
+//   else{
+//     printf("Its Odd number");
+//   }
+//   return 0;
+// }
