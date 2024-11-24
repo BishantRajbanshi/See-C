1b747c42dab3a3cd1dd2a085a6bc2b19f86fc1e4
BishantRajbanshi <rajbancbishant@gmail.com>
2024-11-24 12:17:15 +0545

    nun

---
+++ 1stWorkshop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// int main() {
+//     int i, sum = 0;
+
+//     for (i = 1; i <= 10; i++) { 
+//         sum += i;
+//     }
+//     printf("The sum of numbers from 1 to 10 is: %d", sum); 
+// }
+
+
+int main(){
+  int fact,i;
+
+  printf("Enter the number you want factorial of :%d",i);
+  scanf("%d",&i);
+
+  
+}
