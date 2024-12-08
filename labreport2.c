7e25c26b54e34331b98c4e4ec659881763801144
BishantRajbanshi <rajbancbishant@gmail.com>
2024-12-08 11:16:51 +0545

    changes

--- labreport2.c
+++ labreport2.c
@@ -102,35 +102,113 @@
 // }
 
 
-#include <stdio.h>
-#include <math.h>
+// #include <stdio.h>
+// #include <math.h>
 
-double velocityCalc(double v, double u, double a, double t) {
-    if (isnan(v))
-        return u + a * t;
-    else if (isnan(u))
-        return v - a * t;
-    else if (isnan(a))
-        return (v - u) / t;
-    else if (isnan(t))
-        return (v - u) / a;
-    else {
-        printf("Error: \n");
-        return NAN;
-    }
-}
+// double velocityCalc(double v, double u, double a, double t) {
+//     int nanCount = 0;
 
-int main() {
-    double v = NAN;
-    double u = 20;
-    double a = 4;
-    double t = 5;
+//     if (isnan(v)) nanCount++;
+//     if (isnan(u)) nanCount++;
+//     if (isnan(a)) nanCount++;
+//     if (isnan(t)) nanCount++;
 
-    double calc = velocityCalc(v, u, a, t);
-    if (!isnan(calc))
-        printf("Calculated value: %.2f\n", calc);
-    else
-        printf("Calculation failed\n");
+//     if (nanCount > 1) {
+//         printf("Error: More than one variable cannot be NAN.\n");
+//         return NAN;
+//     }
 
-    return 0;
-}
+//     if (isnan(v))
+//         return u + a * t;
+//     else if (isnan(u))
+//         return v - a * t;
+//     else if (isnan(a))
+//         return (v - u) / t;
+//     else if (isnan(t))
+//         return (v - u) / a;
+//     else {
+//         printf("Error: No variable is NAN.\n");
+//         return NAN;
+//     }
+// }
+
+// int main() {
+//     double v, u, a, t;
+//     int choice;
+
+//     printf("Enter the variable to calculate (1 for v, 2 for u, 3 for a, 4 for t): ");
+//     scanf("%d", &choice);
+
+//     v = u = a = t = NAN;
+
+// switch (choice) {
+//     case 1: 
+//         printf("Enter initial velocity (u), acceleration (a), and time (t): ");
+//         scanf("%lf %lf %lf", &u, &a, &t);
+//         break;
+        
+//     case 2: 
+//         printf("Enter final velocity (v), acceleration (a), and time (t): ");
+//         scanf("%lf %lf %lf", &v, &a, &t);
+//         break;
+        
+//     case 3: 
+//         printf("Enter final velocity (v), initial velocity (u), and time (t): ");
+//         scanf("%lf %lf %lf", &v, &u, &t);
+//         break;
+        
+//     case 4: 
+//         printf("Enter final velocity (v), initial velocity (u), and acceleration (a): ");
+//         scanf("%lf %lf %lf", &v, &u, &a);
+//         break;
+        
+//     default:
+//         printf("Invalid choice.\n");
+//         return 0;
+// }
+
+
+//     double result = velocityCalc(v, u, a, t);
+//     if (!isnan(result)) {
+//         switch (choice) {
+//             case 1: 
+//             printf("Final velocity (v) = %.2f\n", result); 
+//             break;
+//             case 2: 
+//             printf("Initial velocity (u) = %.2f\n", result); 
+//             break;
+//             case 3: 
+//             printf("Acceleration (a) = %.2f\n", result); 
+//             break;
+//             case 4: 
+//             printf("Time (t) = %.2f\n", result); 
+//             break;
+//         }
+//     } else {
+//         printf("Calculation failed due to invalid input.\n");
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// void equations(double a, double b, double c, double d, double e, double f) {
+//     double x, y;
+//     double det = a * e - b * d;
+
+//     if (det == 0) {
+//         printf("The equations have no unique solutions\n");
+//         return;
+//     }
+
+//     x = (c * e - b * f) / det;
+//     y = (a * f - c * d) / det;
+
+//     printf("The solution is: x = %.2f, y = %.2f\n", x, y);
+// }
+
+// int main() {
+//     equations(5,4,7,-9,8,5);
+//     return 0;
+// }
